8cd105e5ac658399f1de84bfd25a770a7180b93b
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2025-11-09 19:18:13 +0300

    test

---
+++ docs/main.js
@@ -0,0 +1,12 @@
+import { Header } from "../src/components/header/header.js";
+import { Main } from "../src/page/home/main/main";
+import { Footer } from "../src/components/footer/footer";
+
+function app() {
+  Header();
+  Main();
+  Footer();
+  console.log("hell");
+}
+app();
+// -----------------------------------------------------------
